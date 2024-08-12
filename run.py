c488ed273f719ebfcd473a9adbefeae27473cbcb
Ozi-cheri Ihemedu <silvernasa@myyahoo.com>
2024-08-12 20:13:37 +0000

    Add python main method,prints funtion

--- run.py
+++ run.py
@@ -1,2 +1,23 @@
+tasks = []
+
+if __name__ == "__main__":
+    ### Create a loop to run the app
+    print("Welcome to the to do list app :)")
+    while True:
+        print("\n")
+        print("Please select one of the following options")
+        print("------------------------------------------")
+        print("1. Add a new task")
+        print("2. Delete a task")
+        print("3. List task")
+        print("4. Quit")
+
+        
+        
+        
+
+
+
+
 
         
